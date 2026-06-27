9010fd0976b15c8cf430690a9e5e3f3dcfa83cee
3K-avya <kavyasahu6@gmail.com>
2026-06-27 23:05:55 +0530

    Fix initial authentication routing

--- backend/src/lib/generateToken.js
+++ backend/src/lib/generateToken.js
@@ -6,11 +6,11 @@ export const generateToken = (userId, res) => {
   })
 
   res.cookie("jwt", token, {
-    maxAge: 7*24*60*60*1000,  // in milliseconds
-    httpOnly: true,  //prevents XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development"
-  })
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV !== "development",
+  });
 
   return token;
 }
--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,31 +1,43 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from './components/HomePage'
 import LoginPage from './components/LoginPage'
 import SignupPage from './components/SignupPage'
 import NavBar from './components/NavBar'
 
+
 function App() {
+  const [loading, setLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
   const checkLogin = async () => {
     try {
       await axios.get(
-         `${import.meta.env.VITE_API_URL}/auth/profile`,
+        `${import.meta.env.VITE_API_URL}/auth/profile`,
         {
           withCredentials: true,
         }
       );
-
       setIsLoggedIn(true);
     } catch {
       setIsLoggedIn(false);
+    } finally {
+      setLoading(false);
     }
   };
-  checkLogin();
-}, []);
+    checkLogin();
+  }, []);
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-700">
+        <h1 className="text-3xl font-bold text-amber-100">
+          Loading...
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -33,7 +45,7 @@ function App() {
       setIsLoggedIn={setIsLoggedIn}/>
 
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route path="/" element={isLoggedIn ? (<HomePage />) : (<Navigate to="/login" replace />)}/>
         <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>}/>
         <Route path="/signup" element={<SignupPage setIsLoggedIn={setIsLoggedIn}/>}/>
       </Routes>
